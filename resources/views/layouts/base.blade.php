1f78ea451f7ecc1763009484e5781629ad428b21
nicolafelice <felicenicola99@hotmail.com>
2021-11-11 14:27:19 +0100

    add header nav

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,6 +9,26 @@
 </head>
 <body>
 
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">laravel-comics</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="{{route("comics.index")}}">Lista fumetti</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route("comics.create")}}">Crea un nuovo fumetto</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
   <main>
     <div class="container">
       @yield('pageContent')
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.base');
-});
+Route::get('/', 'ComicController@index');
 
 Route::resource('comics', 'ComicController');
