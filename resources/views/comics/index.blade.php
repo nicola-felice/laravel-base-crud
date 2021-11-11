e32524e2bfa0569e62e26e044a46060a314f8565
nicolafelice <felicenicola99@hotmail.com>
2021-11-11 17:47:39 +0100

    bonus: warning on delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -24,11 +24,33 @@
 		<td>
 			<a href="{{route("comics.show", $comic["id"])}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
 			<a href="{{route("comics.edit", $comic["id"])}}"><button type="button" class="btn btn-secondary mt-2">Modifica</button></a>
-			<form class="mt-2" action="{{route("comics.destroy", $comic["id"])}}" method="POST">
-				@csrf
-				@method("DELETE")
-				<button type="submit" class="btn btn-danger">Elimina</button>
-			</form>
+			
+			<button data-toggle="modal" type="button" class="btn btn-danger mt-2" data-target="#modal{{$comic["id"]}}">Elimina</button>
+			{{-- modal --}}
+			<div class="modal" tabindex="-1" id="modal{{$comic["id"]}}">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title">Attenzione!</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<p>Sei sicuro di voler eliminare il fumetto?</p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+							<form action="{{route("comics.destroy", $comic["id"])}}" method="POST">
+								@csrf
+								@method("DELETE")
+								<button type="submit" class="btn btn-danger">Elimina</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			{{-- end modal --}}
 		</td>
 	  </tr>
 	  @endforeach
