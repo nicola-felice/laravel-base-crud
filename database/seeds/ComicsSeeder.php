f5c33b685aa3617826dae325c6d2086676915854
nicolafelice <felicenicola99@hotmail.com>
2021-11-11 13:43:51 +0100

    add: seeder

---
+++ database/seeds/ComicsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+
+        foreach($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic["title"];
+            $newComic->type = $comic["type"];
+            $newComic->description = $comic["description"];
+            $newComic->thumb = $comic["thumb"];
+            $newComic->price = $comic["price"];
+            $newComic->series = $comic["series"];
+            $newComic->sale_data = $comic["sale_date"];
+            $newComic->save();
+        }
+    }
+}
